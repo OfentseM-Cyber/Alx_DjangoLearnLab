af15bcefeca7aefd721b82f6382a97a16ba65b09
Ofentse <fentsemosiane@gmail.com>
2025-09-06 13:26:24 +0200

    Complete user authentication with login, logout, and registration

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -118,3 +118,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/relationship/books/'
+LOGOUT_REDIRECT_URL = '/relationship/login/'
